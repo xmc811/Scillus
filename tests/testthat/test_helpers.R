b2849021c05ab64ed1800560b4f97bb519539c59
Mingchu Xu <mxu.china@gmail.com>
2019-08-26 12:18:55 -0500

    minor updates

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -1,6 +1,7 @@
 
-context("Get colors")
+context("Test helpers")
 library(Scillus)
+
 
 test_that("Correct output of get_colors", {
         expect_equal(get_colors(v = c(1,3), pal = "Set1"), c("#E41A1C", "#4DAF4A"))
